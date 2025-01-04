7c08504f5773ad26e9ced35d7b08d7ac78625fe5
gaxpar <anndremarttins@gmail.com>
2025-01-04 08:55:44 -0300

    Configuração do MapView (Tentar entender porque Callout não funciona)

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
-import { View, Alert } from "react-native";
-import MapView from "react-native-maps";
-
+import { View, Alert, Text } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
 
 import { api } from "@/services/api";
+import { fontFamily, colors } from "@/styles/theme"
 
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
 import { Categories, CategoriesProps } from "@/components/categories";
 
-type MarketsProps = PlaceProps
+type MarketsProps = PlaceProps & {
+  latitude: number
+  longitude: number
+}
 
 const currentLocation = {
   latitude: -23.561187293883442,
@@ -63,15 +66,65 @@ export default function Home() {
         selected={category}
       />
 
-      <MapView 
-      style={{ flex: 600 }}
-      initialRegion={{
-        latitude: currentLocation.latitude,
-        longitude: currentLocation.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
-      />
+      <MapView
+        style={{ flex: 600 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {
+          markets.map((item) => (
+            <Marker
+              key={item.id}
+              identifier={item.id}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+              image={require('@/assets/pin.png')}
+            >
+            
+              <Callout>
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      color: colors.gray[600],
+                      fontFamily: fontFamily.medium
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      color: colors.gray[600],
+                      fontFamily: fontFamily.regular
+                    }}
+                  >
+                    {item.address}
+                  </Text>
+                </View>
+              </Callout>
+            </Marker>
+          ))
+        }
+
+      </MapView>
 
       <Places data={markets} />
     </View>
